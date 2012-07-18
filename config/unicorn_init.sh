527537e283ac00a1fb92005cdb6a204b074e9563
Breno Salgado <salgado.breno@gmail.com>
2012-07-18 00:42:42 -0300

    using mock env

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/blog/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E mock"
 AS_USER=deployer
 set -u
 
