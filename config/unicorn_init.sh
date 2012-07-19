e7ddd0ea7089396886bbb657117859bd61066d85
Breno Salgado <salgado.breno@gmail.com>
2012-07-19 07:29:57 -0300

    unicorn_init fixes

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/blog/current
+APP_ROOT=/home/ubuntu/apps/fubuia/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E mock"
-AS_USER=deployer
+AS_USER=ubuntu
 set -u
 
 OLD_PIN="$PID.oldbin"
