4a7fe5fc55567cb955e6126873ebcbab7a39502c
Breno Salgado <salgado.breno@gmail.com>
2012-10-23 23:10:08 -0200

    trynna fix exception when there's no tagging or event

--- config/initializers/most_recent_updated_at_extension.rb
+++ config/initializers/most_recent_updated_at_extension.rb
@@ -1,9 +1,9 @@
 class ActiveRecord::Base
   def self.most_recent_updated_at
-    select("updated_at").order("updated_at DESC").limit(1).first.updated_at
+    select("updated_at").order("updated_at DESC").limit(1).first.try(:updated_at)
   end
 
   def self.most_recent_created_at
-    select("created_at").order("created_at DESC").limit(1).first.created_at
+    select("created_at").order("created_at DESC").limit(1).first.try(:created_at)
   end
 end
